f3e31699f23a444d3aa52602f5430808d6173e2e
Caveman Frak <tim.pickard@bluegecko.co.uk>
2023-04-01 19:44:35 +0100

    refactor geo data add downloader

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
 }
 
 dependencies {
+    implementation("org.springframework.data:spring-data-commons")
     implementation("org.locationtech.spatial4j:spatial4j:0.8")
     implementation("tech.uom.lib:uom-lib-jackson:2.1")
 }
