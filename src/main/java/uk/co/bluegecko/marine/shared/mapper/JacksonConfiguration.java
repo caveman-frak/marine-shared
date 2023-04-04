0109ff21eb5269cf2cc2fcc5cff016b3cb26c758
Caveman Frak <tim.pickard@bluegecko.co.uk>
2023-04-04 22:15:01 +0100

    fix misc warnings

--- src/main/java/uk/co/bluegecko/marine/shared/mapper/JacksonConfiguration.java
+++ src/main/java/uk/co/bluegecko/marine/shared/mapper/JacksonConfiguration.java
@@ -2,9 +2,10 @@ package uk.co.bluegecko.marine.shared.mapper;
 
 import com.fasterxml.jackson.databind.JsonDeserializer;
 import com.fasterxml.jackson.databind.JsonSerializer;
-
 import javax.measure.Quantity;
 import javax.measure.Unit;
+import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
+import org.springframework.context.annotation.Bean;
 
 /**
  * Jackson object mappers configuration, including support for Unit of Measure.
@@ -16,12 +17,11 @@ public class JacksonConfiguration {
 	/**
 	 * Add object mapper support for Unit Of Measure.
 	 *
-	 * @return builder with {@link JsonSerializer}s and {@link JsonDeserializer}s
-	 * for {@link Unit} and {@link Quantity}.
+	 * @return builder with {@link JsonSerializer}s and {@link JsonDeserializer}s for {@link Unit} and {@link Quantity}.
 	 */
-//	@Bean
-//	public Jackson2ObjectMapperBuilderCustomizer registerUnitOfMeasureModule() {
-//		return builder -> builder.modules(new MarineJacksonModule())
-//	}
+	@Bean
+	public Jackson2ObjectMapperBuilderCustomizer registerUnitOfMeasureModule() {
+		return builder -> builder.modules(new MarineJacksonModule());
+	}
 
 }
